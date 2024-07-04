3cf0e77e8eb64da9440c658c91b1975601778d59
CheerUpBee <137815765+CheerUpBee@users.noreply.github.com>
2024-07-05 05:52:47 +0900

    Update DilatedCausal1DConv.v
    add multiple I/O channel

--- DilatedCausal1DConv.v
+++ DilatedCausal1DConv.v
@@ -9,72 +9,110 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module DilatedCausal1DConv(
+module DilatedCausal1DConv #(
+    parameter integer DILATION_FACTOR = 1,
+    parameter integer KERNEL_SIZE = 2,
+    parameter integer INPUT_CHANNELS = 512,
+    parameter integer OUTPUT_CHANNELS = 512
+)(
     input clk,
     input reset,
-    input wire signed [7:0] x, // 8-bit input signal
-    output reg signed [7:0] y // 8-bit output signal
+    input wire signed [7:0] x[0:INPUT_CHANNELS-1], // 8-bit input signal
+    output reg signed [7:0] y[0:OUTPUT_CHANNELS-1], // 8-bit output signal
+    
+    input h_write,
+    input [7:0] h_index_channel,
+    input [7:0] h_index_filter,
+    input [7:0] h_value    
     );
     
-    parameter [3:0] dilation_factor = 4'd2;
-    parameter integer KERNEL_SIZE = 3;
+
     
-    reg signed [7:0] h [0:KERNEL_SIZE-1];
+    reg signed [7:0] h [0:INPUT_CHANNELS-1][0:KERNEL_SIZE-1];
     
-    reg signed [7:0] x_delay [0:(dilation_factor*(KERNEL_SIZE-1))];
+    reg signed [7:0] x_delay [0:INPUT_CHANNELS-1][0:(DILATION_FACTOR*(KERNEL_SIZE-1))];
     
-    reg signed [15:0] mul [0:KERNEL_SIZE-1];
+    reg signed [15:0] mul [0:INPUT_CHANNELS-1][0:KERNEL_SIZE-1];
     
-    reg signed [15:0] sum;
+    reg signed [15:0] sum [0:OUTPUT_CHANNELS-1];
     
-    integer i;
+    integer i,j;
     
-    initial begin
-        for (i=0;i<KERNEL_SIZE;i=i+1)begin
-            h[i]=8'sh01+i; // just for test
-        end
-    end
     
     always @(posedge clk or posedge reset) begin
         if(reset) begin
-            for (i = 0; i <= (dilation_factor*(KERNEL_SIZE-1)); i = i + 1) begin
-                x_delay[i] <= 8'sh0;
+        for(i = 0; i <= INPUT_CHANNELS-1; i= i+1) begin
+            for (j = 0; j <= (DILATION_FACTOR*(KERNEL_SIZE-1)); j = j + 1) begin
+                x_delay[i][j] <= 8'sh0;
             end           
-            sum <= 16'sh0;
-            y <= 8'sh0;  
+            sum[i] <= 16'sh0; 
         end
+    end
         
         else begin
-            for(i=(dilation_factor*(KERNEL_SIZE-1)); i>0; i=i-1) begin
-                x_delay[i] <= x_delay[i-1];
+        for(i = 0; i <= INPUT_CHANNELS-1; i= i+1) begin
+            for(j=(DILATION_FACTOR*(KERNEL_SIZE-1)); j>0; j=j-1) begin
+                x_delay[i][j] <= x_delay[i][j-1];
             end
-            x_delay[0] <= x;
+            x_delay[i][0] <= x[i][0];
         end
     end
-always @(*) begin
-    sum = 0;
-    for (i = 0; i < KERNEL_SIZE; i = i + 1) begin
-        sum = sum + mul[i];
-    end
 end
-
-always @(*) begin
-    for(i=0; i< KERNEL_SIZE; i = i + 1) begin
-        mul[i] = x_delay[dilation_factor * i] * h[i];
-    end
-end
-
-always @(*) begin
-        if(sum >= 127) begin
-            y <= 8'd127;
+    
+    always @ (posedge clk or posedge reset) begin // filter coef.
+        if(reset) begin
+            for(i = 0; i <= INPUT_CHANNELS-1; i= i+1) begin
+                for(j=0; j<KERNEL_SIZE; j=j+1) begin
+                    h[i][j] <= 8'd1 + j;
+                end
+            end
         end
         
-        else if(sum <=-128) begin
-            y <= -8'd128;
-        end
         else begin
-            y <= sum[7:0];
+            if(h_write == 1) begin
+                h[h_index_channel][h_index_filter] <= h_value;
+            end
+        end
+    end    
+    
+always @(*) begin
+    for(i = 0; i <= INPUT_CHANNELS-1; i= i+1) begin
+        sum[i] = 0;
+        for (j = 0; j < KERNEL_SIZE; j = j + 1) begin
+            sum[i] = sum[i] + mul[i][j];
+        end
+     end
+end
+
+always @(*) begin
+    for(i = 0; i <= INPUT_CHANNELS-1; i= i+1) begin
+        for(j=0; j< KERNEL_SIZE; j = j + 1) begin
+            mul[i][j] = x_delay[i][DILATION_FACTOR * j] * h[i][j];
+        end
+    end
+end
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        for(i = 0; i <= OUTPUT_CHANNELS-1; i= i+1) begin
+            y[i] = 8'sh0;
+        end
+    end
+    
+    else begin
+        for(i = 0; i <= OUTPUT_CHANNELS-1; i= i+1) begin
+            if(sum[i] >= 127) begin
+                y[i] <= 8'd127;
+            end
+        
+            else if(sum[i] <=-128) begin
+                y[i] <= -8'd128;
+            end
+            else begin
+                y[i] <= sum[i][7:0];
+            end
         end    
+    end
 end
     
 endmodule
